e95da1d263e950a79904d28f9f45bd59ccbe7233
matt.king <matt.king@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-13 12:19:47 +0000

    DribbblePortfolioShotsWidget
    git-svn-id: https://plugins.svn.wordpress.org/dribbble-portfolio-shots-widget/trunk@1088916 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- dribbble-portfolio-shots-widget.php
+++ dribbble-portfolio-shots-widget.php
@@ -82,7 +82,7 @@ class DribbblePortfolioShotsWidget extends WP_Widget{
                  get_dribbble(playerId,column);
             </script>
        ";
-	   $data .= "<div style='color:#ccc; font-size: 9px; text-align:right;'><a href='http://www.crayfishstudios.com/' title='click here' target='_blank'>dallas web designer</a></div>";
+	   $data .= "<div style='color:#ccc; font-size: 9px; text-align:right;'><a href='http://www.crayfishstudios.com/' title='Crayfish Studios' target='_blank'>Crayfishstudios.com</a></div>";
         echo $before_widget;
         echo $before_title . $title . $after_title;
             echo $data;
